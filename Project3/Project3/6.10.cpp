3fb945afe3524cb075d3ccd59555b4b5c9c3701d
MuqiuWhite <1219214097@qq.com>
2021-06-10 16:08:35 +0800

    c

--- Project3/Project3/6.10.cpp
+++ Project3/Project3/6.10.cpp
@@ -16,18 +16,19 @@ int main()
 	//printf("%s,%d,%s,%s", his.name, his.age, his.sex, his.bir);
 	//struct he* p = &his;
 
+#include <stdio.h>
+    int main()
+    {
         int n = 0, x = 0, key = 0, i = 0;
-        int a[100] = { 0 }, count = 0;
-
+        int count = 0;
         scanf("%d", &n);
-
+        int a[n];
         for (i = 0; i < n; i++)
         {
-            scanf("%d", a[i]);
+            scanf("%d", &a[i]);
         }
 
         scanf("%d", &key);
-
         for (i = 0; i < n; i++)
         {
             if (a[i] == key)
@@ -36,6 +37,49 @@ int main()
             }
         }
         printf("%d", count);
+
+        int x, a;
+        char p;
+        while (scanf("%d", &x) != EOF)
+        {
+            a = x / 10;
+            switch (a)
+            {
+            case 0:
+            case 1:
+            case 2:
+            case 3:
+            case 4:
+            case 5:
+                p = 'E';
+                break;
+            case 6:
+                p = 'D';
+                break;
+            case 7:
+                p = 'C';
+                break;
+            case 8:
+                p = 'B';
+                break;
+            case 9:
+            case 10:
+                p = 'A';
+                break;
+            default:
+                p = 'F';
+                break;
+
+            }
+            if (x > 100 || x < 0)
+            {
+                p = 'F';
+            }
+            printf("%c", p);
+
+        }
+        return 0;
+    }
 
 	return 0;
 }
