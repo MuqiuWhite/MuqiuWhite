19e553c18448933d9d5278faa5cc73f06d521c84
MuqiuWhite <1219214097@qq.com>
2021-06-14 20:44:20 +0800

    c

--- 6.13/6.13/源.cpp
+++ 6.13/6.13/源.cpp
@@ -1,11 +1,42 @@
 #define _CRT_SECURE_NO_WARNINGS 1
+//#define a 10
+#include <string.h>
 #include <stdio.h>
+//int b = 2;
+//enum people
+//{
+//	name,
+//	sex
+//};
 int main()
 {
-	int a = 0;//初始化变量
-	int b = 0;
-	scanf("%d%d", &a, &b);
-	printf("%d,&d\n", a, b);
+	//char ch1[] = "abc";
+	//printf("%d\n",sizeof(ch1));
+	//printf("%d\n",strlen(ch1));
+	char ch3[] = { 'b', 'i', 't', '\0' };
+	printf("%d\n", strlen(ch3));
+	//char ch2[4] = { 'a','b','c' };
+	//printf("%d\n", sizeof(ch2));
+	//char ch3[] = { 'a','b','c' };
+	//printf("%d\n", sizeof(ch3));
 
-	return 0;
+//	enum people ww = name;
+	//int a = 1;
+	//
+	//{
+	//	int c = 3;
+	//}
+	//printf("%d\n", a);
+	//printf("%d\n", b);
+	//printf("%d\n", c);
+
+	/*int a = 1;
+	char b = 'b';
+	float c = 1.3;*/
+	
+	/*const int a = 1;
+	a = 2;*/
+
+return 0;
 }
+
