af61ed9f5ebcfbdbb9c740d4f4ccd172ff2e972a
MuqiuWhite <1219214097@qq.com>
2021-07-27 20:11:32 +0800

    C

---
+++ 7.27/7.27/源.cpp
@@ -0,0 +1,125 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+//#include <stdio.h>
+//#include <string.h>
+//void str(char* a)
+//{
+//	int z =strlen(a);
+//	printf("%d", z);
+//}
+//int up(int n)
+//{
+//	if (n < 12 && n >= 0)
+//	{
+//		return 2;
+//	}
+//	else
+//	{		
+//			return 4* (n / 12)+2;	
+//	}
+//}
+//long sum(long n, long m)
+//{
+//	long c = n * m;
+//	long r = 0;
+//	while (n%m)
+//	{
+//		r = n % m;
+//		n = m;
+//		m = r;
+//	}
+//	return c / m + m;
+//}
+		//int i, j;
+		//for (i = 0; i < n; i++)
+		//{
+		//	for (j = 0; j < n; j++)
+		//	{
+		//		if ((i < n-1&&i>0) && (j < n-1&&j>0))
+		//		{
+		//			printf("  ");
+		//			continue;
+		//		}
+		//		if (j == 0 )
+		//			printf("*");
+		//		else
+		//		{
+		//			printf(" *");
+		//		}
+
+		//	}
+		//	printf("\n");
+#include <stdio.h>
+#include <string.h>
+
+int main()
+{
+
+	return 0;
+}
+
+//int a, b, c;
+//while (scanf("%d%d%d",&a,&b,&c)!=EOF)
+//{
+//	if (a + b > c && a + c > b && b + c > a)
+//	{
+//		if (a == b && b== c && a==c)
+//			printf("Equilateral triangle!\n");
+//		else if (a == b || b == c || a == c)
+//			printf("Isosceles triangle!\n");
+//		else
+//			printf("Ordinary triangle!\n");
+//	}
+//	else
+//		printf("Not a triangle!\n");
+
+//}
+
+//int i;
+//float sum = 0, max = 0, min = 100;
+//for (i = 0; i < 7; i++)
+//{
+//	scanf("%f", &a[i]);
+//	if (a[i] > max)
+//		max = a[i];
+//	if (a[i] < min)
+//		min = a[i];
+//	sum = sum + a[i];
+//}
+
+//float score = (sum - max - min) / 5;
+//printf("%.2f", score);
+
+//getchar();
+//if ((n >= 'a' && n <= 'z') || (n >= 'A' && n <= 'Z'))
+//	printf("YES\n");
+//else
+//	printf("NO\n");
+/*long s = sum(n, m);*/
+//long n = 0, m = 0;
+//int time=up(n);
+//printf("%d\n", time);
+//int a = 0; 
+//int i = 0;
+//while (scanf("%d", &a) != EOF)
+//{
+//	int c[100] = { 0 };
+//	int size = 0;
+//	while (a)
+//	{
+//		c[size] = a % 6;
+//		a = a / 6;
+//		size++;
+//	}
+//	for (i = size-1; i >= 0; i--)
+//	{
+//		printf("%d", c[i]);
+//	}
+//	printf("\n");
+/*char a[6] = { 1,2,3,4,5 };
+	str(a);*/
+	/*int a[3] = { 1,2,3 };
+	int* p = a;
+	printf("%p\n", &a + 1);
+	printf("%p\n", &a[2]);
+	printf("%p\n", &a[0]);
+	printf("%p\n", &a[2]+1);*/
