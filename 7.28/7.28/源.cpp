17d9850dc888cf69e9ed01d91acb92cdc0fccd85
MuqiuWhite <1219214097@qq.com>
2021-07-28 15:31:30 +0800

    C

---
+++ 7.28/7.28/源.cpp
@@ -0,0 +1,21 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include <stdio.h> 
+#include <string.h> 
+int main()
+{
+	//int a[2] = { 1,2 };
+	//int b[5] = { 3 };
+	//int i;
+//	memcpy(b, a, sizeof(int) * 3);
+	int n = 0;
+	char s[20];
+	while (scanf("%s", s))
+	{
+		for (i = 0; i < 5; i++)
+		{
+
+			printf("%d", b[i]);
+		}
+	}
+	return 0;
+}
